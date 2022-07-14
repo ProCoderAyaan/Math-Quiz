83b9734f3f617307ccf35ae82d636142f6b92f4e
ProCoderAyaan <95852791+ProCoderAyaan@users.noreply.github.com>
2022-07-14 13:42:06 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -42,3 +42,37 @@ function submit() {
 }
 question_turn = "player1";
 answer_turn = "player2";
+function check(){
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+    console.log("answer in lower case - " + answer);
+    if(answer == word){
+        if(answer_turn == "player1"){
+            player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else{
+            player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = player2_score;
+        }
+    }
+    if(question_turn == "player1"){
+        question_turn = "player2"
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name ;
+    }
+    else{
+        question_turn = "player1"
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+    }
+
+    if(answer_turn == "player1"){
+        answer_turn = "player2"
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+    }
+    else{
+        answer_turn = "player1"
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ;
+    }
+
+    document.getElementById("output").innerHTML = "";
+}
